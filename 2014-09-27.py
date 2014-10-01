204c9365ffc3cf5d24034e20bf8165a93708d370
Matt Warren <matt.warren@gmail.com>
2014-09-30 20:47:00 -0600

    re-indent to 4 spaces. add check for existing bucket names

--- 2014-09-27.py
+++ 2014-09-27.py
@@ -57,37 +57,46 @@ questions = [
 
 
 def s3_bucket_maker( answers ):
-  access_key = os.environ[answers['org'] + '_ACCESS_KEY_ID']
-  secret_key = os.environ[answers['org'] + '_SECRET_ACCESS_KEY']
-  s3conn = S3Connection(access_key, secret_key)
-  iamconn = IAMConnection(access_key, secret_key)
+    access_key = os.environ[answers['org'] + '_ACCESS_KEY_ID']
+    secret_key = os.environ[answers['org'] + '_SECRET_ACCESS_KEY']
+    s3conn = S3Connection(access_key, secret_key)
+    iamconn = IAMConnection(access_key, secret_key)
 
-  bucket = s3conn.create_bucket(answers['bucket_name'])
+    bucket = s3conn.lookup(answers['bucket_name'])
+    if bucket:
+        print 'bucket (%s) already exists' % answers['bucket_name']
+    else:
+        try:
+            bucket = s3conn.create_bucket(answers['bucket_name'])
+        except boto.exception.S3CreateError, e:
+            print 'Bucket (%s) is owned by another user' % answers['bucket_name']
+            raise e
 
-  user = None
-  try:
-    user = iamconn.get_user(answers['username'])
-  except boto.exception.BotoServerError, e:
-    if e.status == 404:
-      print 'User not found... creating one'
-      user = iamconn.create_user(answers['username'])
-      keys = iamconn.create_access_key(answers['username'])
-      print keys
-    else :
-      raise e
-  print user
 
-  policy = key_policy_json % (user.arn)
-  user_policy = iamconn.put_user_policy(answers['username'], 'UserKeyPolicy', policy)
+    user = None
+    try:
+        user = iamconn.get_user(answers['username'])
+    except boto.exception.BotoServerError, e:
+        if e.status == 404:
+            print 'User not found... creating one'
+            user = iamconn.create_user(answers['username'])
+            keys = iamconn.create_access_key(answers['username'])
+            print keys
+        else :
+            raise e
+    print user
 
-  actions = "[\"s3:*\"]"
-  if (answers['acl'] == 'read'):
-    actions = "[\"s3:ListBucket\",\"s3:GetObject\",\"s3:GetObjectVersion\"]"
-  policy = bucket_policy_json % (actions, answers['bucket_name'], answers['bucket_name'], answers['bucket_name'], answers['bucket_name'])
-  bucket_policy = iamconn.put_user_policy(answers['username'], 'UserS3Policy', policy)
+    policy = key_policy_json % (user.arn)
+    user_policy = iamconn.put_user_policy(answers['username'], 'UserKeyPolicy', policy)
+
+    actions = "[\"s3:*\"]"
+    if (answers['acl'] == 'read'):
+        actions = "[\"s3:ListBucket\",\"s3:GetObject\",\"s3:GetObjectVersion\"]"
+    policy = bucket_policy_json % (actions, answers['bucket_name'], answers['bucket_name'], answers['bucket_name'], answers['bucket_name'])
+    bucket_policy = iamconn.put_user_policy(answers['username'], 'UserS3Policy', policy)
 
 
 if __name__=='__main__':
 
-  answers = inquirer.prompt(questions)
-  s3_bucket_maker(answers)
+    answers = inquirer.prompt(questions)
+    s3_bucket_maker(answers)
