8b6c5a29b9707ffaedd17643c802f31e24290d55
Matt Warren <matt.warren@gmail.com>
2014-10-02 22:05:07 -0600

    implement quicksort

--- fc_2014_10_02.py
+++ fc_2014_10_02.py
@@ -47,3 +47,49 @@ class UI:
 if __name__=='__main__':
     ui = UI()
     ui.print_names()
+
+
+
+
+
+
+##
+#
+# Quicksort
+#
+
+
+
+import random
+
+numbers = random.sample(xrange(100), 100)
+
+def swap(numbers, i, j):
+    temp = numbers[j]
+    numbers[j] = numbers[i]
+    numbers[i] = temp
+
+def pivot(numbers, start_index, end_index):
+    q = numbers[start_index]
+    i = start_index + 1
+    j = end_index
+    while i <= j:
+        if numbers[i] <= q:
+            i = i+1
+        elif numbers[i] > q:
+            swap(numbers, i, j)
+            j = j - 1
+    swap(numbers, start_index, i-1)
+    if (i-2) > start_index:
+        pivot(numbers,start_index, i-2)
+    if end_index > i:
+        pivot(numbers, i, end_index)
+
+print numbers
+pivot(numbers,0,len(numbers)-1)
+print numbers
+
+# test_numbers = random.sample(xrange(10),3)
+# print test_numbers
+# swap(test_numbers, 1,2)
+# print test_numbers
