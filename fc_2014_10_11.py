4870c92aaf903f642e0eba27452cb2951a2cdfb3
Matt Warren <matt.warren@gmail.com>
2014-10-11 22:09:54 -0600

    adding the private key to the script

--- fc_2014_10_11.py
+++ fc_2014_10_11.py
@@ -11,11 +11,13 @@ import shutil
 # This script switches out my public keys so I can use two GitHub accounts
 
 PUBLIC_KEY_FILE = '~/.ssh/id_rsa.pub'
+PRIVATE_KEY_FILE = '~/.ssh/id_rsa'
 
 print """##
-## This script assumes you have multiple public keys on the computer
+## This script assumes you have multiple keys on the computer
 ##
 ## create files ~/.ssh/id_rsa.pub.PERSONAL and ~/.ssh/id_rsa.pub.WORK
+## and files ~/.ssh/id_rsa.PERSONAL and ~/.ssh/id_rsa.WORK
 ##
 """
 
@@ -29,16 +31,19 @@ if __name__=='__main__':
     try:
         answers = inquirer.prompt(questions)
         PUBLIC_KEY_FILE = os.path.expanduser(PUBLIC_KEY_FILE)
-        file_to_use = '%s.%s' % (PUBLIC_KEY_FILE, answers['keys'])
+        PRIVATE_KEY_FILE = os.path.expanduser(PRIVATE_KEY_FILE)
+        public_file_to_use = '%s.%s' % (PUBLIC_KEY_FILE, answers['keys'])
+        private_file_to_use = '%s.%s' % (PRIVATE_KEY_FILE, answers['keys'])
 
         print "Switching to %s keys" % answers['keys']
 
-        if os.path.isfile(file_to_use):
-            shutil.copyfile(file_to_use, PUBLIC_KEY_FILE)
+        if os.path.isfile(public_file_to_use) and os.path.isfile(private_file_to_use):
+            shutil.copyfile(public_file_to_use, PUBLIC_KEY_FILE)
+            shutil.copyfile(private_file_to_use, PRIVATE_KEY_FILE)
             print "You are now using your %s keys" % answers['keys']
         else:
-            print "Missing %s public key." % answers['keys']
-            print "Download and copy to %s" % file_to_use
+            print "Missing %s keys" % answers['keys']
+            print "Download and copy to %s" % public_file_to_use
     except KeyboardInterrupt:
         print "Cancelled"
         pass
