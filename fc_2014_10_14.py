1f5cc0683fd8c1387ef2bec4174d6323349b29be
Matt Warren <matt.warren@gmail.com>
2014-10-14 20:25:04 -0600

    a couple of tweaks to the script and fixes found when setting it up on a server

--- fc_2014_10_14.py
+++ fc_2014_10_14.py
@@ -11,6 +11,7 @@ from github import Github
 # Free Coding session for 2014-10-14
 # Written by Matt Warren
 #
+EMAIL = 'me@example.com'
 hub = Github()
 
 hub_user = hub.get_user('mfwarren')
@@ -19,12 +20,13 @@ event = hub_user.get_public_events()[0]  # the most recent public event
 
 last_event_time = event.created_at
 last_event_time = last_event_time + datetime.timedelta(hours=-6) # GMT To Mountain Time
+today = datetime.datetime.now() + datetime.timedelta(hours=-6)
 
-if last_event_time.day != datetime.date.today().day:
+if last_event_time.day != today.day:
     msg = MIMEText("You haven't made any commits to GitHub yet today!")
     msg['Subject'] = "GITHUB ALERT!"
-    msg['From'] = 'me@example.com'
-    msg['To'] = 'me@example.com'
+    msg['From'] = EMAIL
+    msg['To'] = EMAIL
     s = smtplib.SMTP('localhost')
-    s.sendmail(me, [you], msg.as_string())
+    s.sendmail(EMAIL, [EMAIL], msg.as_string())
     s.quit()
