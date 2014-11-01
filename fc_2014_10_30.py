7db9829a41db468f96262d010c54c7b8403c5545
Matt Warren <matt.warren@gmail.com>
2014-11-01 16:59:57 -0600

    refactor historical stock loading into function for future use

--- fc_2014_10_30.py
+++ fc_2014_10_30.py
@@ -11,19 +11,21 @@ import csv
 # Written by Matt Warren
 #
 
-start = datetime(2010, 1, 1)
-end = datetime.today()
+def load_historical_data(start=datetime(2010, 1, 1), end=datetime.today(), filename='stock_data.h5'):
+    store = HDFStore(filename)
 
-store = HDFStore('stock_data.h5')
+    with open('companylist.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            print row[0]
+            try:
+                stock_info = web.DataReader(row[0], "yahoo", start, end)
+                store[row[0]] = stock_info
+            except:
+                print "Error on", row[0]
 
-with open('companylist.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in spamreader:
-        print row[0]
-        try:
-            stock_info = web.DataReader(row[0],  "yahoo", start, end)
-            store[row[0]] = stock_info
-        except:
-            print "Error on", row[0]
+    store.close()
 
-store.close()
+
+if __name__ == '__main__':
+    load_historical_data()
