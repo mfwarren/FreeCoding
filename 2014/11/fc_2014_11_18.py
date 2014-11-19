180c91faad213ad03ef6f8293cacf7118b890577
Matt Warren <matt.warren@gmail.com>
2014-11-18 20:48:30 -0700

    not much time tonight... playing with dates

---
+++ 2014/11/fc_2014_11_18.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# imports go here
+import datetime
+
+#
+# Free Coding session for 2014-11-18
+# Written by Matt Warren
+#
+
+
+def foo():
+    d = datetime.datetime.now()
+
+    print d
+
+    print d + datetime.timedelta(hours=1)
+
+    print d.strftime("%Y-%m-%d")
+    print [(d+datetime.timedelta(hours=i)).strftime("%H:%M:%S") for i in xrange(1,10)]
+
+foo()
