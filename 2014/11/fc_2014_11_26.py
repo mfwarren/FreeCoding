a594f4c0c964a8146595048d149cd618fee9a61a
Matt Warren <matt.warren@gmail.com>
2014-11-26 21:00:31 -0700

    get the hostname

---
+++ 2014/11/fc_2014_11_26.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# imports go here
+from __future__ import print_function
+import socket
+
+#
+# Free Coding session for 2014-11-26
+# Written by Matt Warren
+#
+
+print(socket.gethostname())
