807323535658726b8d75de01a3b9d0d75f242aed
Matt Warren <matt.warren@gmail.com>
2014-11-27 22:27:18 -0700

    playing with numpy

---
+++ 2014/11/fc_2014_11_27.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# imports go here
+from __future__ import print_function
+from numpy import *
+
+#
+# Free Coding session for 2014-11-27
+# Written by Matt Warren
+#
+
+a = arange(24)
+
+print(add.accumulate(a))
+
+
+b = arange(4, 28)
+
+print(a+b)
+
+m = matrix('1 2; 5 8')
+
+print(m*m)
+print(cross(m, m))
