18cde254d1862882da8b1009ce917181a76cfc0a
Matt Warren <matt.warren@gmail.com>
2014-12-15 20:08:02 -0700

    switch start new file to use python3

--- start_new_file.py
+++ start_new_file.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import datetime
 
-file_template = """#!/usr/bin/env python
+file_template = """#!/usr/bin/env python3
 # imports go here
 
 #
@@ -18,9 +18,9 @@ if __name__ == '__main__':
     if not os.path.exists(path):
         os.makedirs(path)
     full_path = os.path.join(path, filename)
-    print full_path
+    print(full_path)
     if os.path.isfile(full_path):
-        print "Today's file is already created"
+        print("Today's file is already created")
     else:
         with open(full_path, 'w') as file:
             file.write(file_template % today.isoformat())
