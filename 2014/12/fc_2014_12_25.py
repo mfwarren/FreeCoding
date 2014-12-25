355a39cea304fc16b40375622c9709e0b94c1d9c
Matt Warren <matt.warren@gmail.com>
2014-12-25 15:19:49 -0700

    use flask-bootstrap to add base template

--- 2014/12/fc_2014_12_25.py
+++ 2014/12/fc_2014_12_25.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # imports go here
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 #
 # Free Coding session for 2014-12-25
@@ -11,11 +12,12 @@ from flask.ext.script import Manager
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return "<h1>Merry Christmas</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     manager.run()
