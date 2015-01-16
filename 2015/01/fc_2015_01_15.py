c76af0248ea4799e451f689c90127aec96b8852f
Matt Warren <matt.warren@gmail.com>
2015-01-15 23:29:05 -0700

    a sad day for free coding :(

---
+++ 2015/01/fc_2015_01_15.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# imports go here
+
+#
+# Free Coding session for 2015-01-15
+# Written by Matt Warren
+#
+
+if __name__ == '__main__':
+    print("No time for free coding today :(")
+    print(":("[::-1])
