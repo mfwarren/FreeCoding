5f066dc94077371b7e1664847daee8e1e52742c8
Matt Warren <matt.warren@gmail.com>
2015-01-30 22:11:49 -0700

    rage coding

---
+++ 2015/01/fc_2015_01_30.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# imports go here
+
+#
+# Free Coding session for 2015-01-30
+# Written by Matt Warren
+#
+
+
+def main():
+    print("writing code for someone else")
+    print("and not getting paid for it")
+    print("with no chance that it benfits me - even indirectly")
+    print("on a friday night")
+    print("this sucks!")
+
+
+if __name__ == '__main__':
+    main()
