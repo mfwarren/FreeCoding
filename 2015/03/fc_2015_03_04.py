a06c783938c9171ac22e73b9fca59479b913e386
Matt Warren <matt.warren@gmail.com>
2015-03-04 23:22:10 -0700

    not much time tonight to write anything

---
+++ 2015/03/fc_2015_03_04.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# imports go here
+import logging
+
+#
+# Free Coding session for 2015-03-04
+# Written by Matt Warren
+#
+
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logger.error("no time to write code tonight :(")
