cf164fe76066997039709f58c8e9b227d7ae07ea
Matt Warren <matt.warren@gmail.com>
2015-03-31 21:49:02 -0600

    sometimes it's hard to think of things to code

---
+++ 2015/03/fc_2015_03_31.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# imports go here
+
+#
+# Free Coding session for 2015-03-31
+# Written by Matt Warren
+#
+
+
+def foo():
+    print("completely out of ideas")
+
+if __name__ == "__main__":
+    foo()
