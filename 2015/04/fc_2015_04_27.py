6548ee2fa50bf35b4c9b326b4e4cc42095469f44
Matt Warren <matt.warren@gmail.com>
2015-04-27 22:03:44 -0600

    sending an email with image attachements

---
+++ 2015/04/fc_2015_04_27.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# imports go here
+import os
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+#
+# Free Coding session for 2015-04-27
+# Written by Matt Warren
+#
+
+
+def send_email(from_email, to_emails):
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Weekly KPI Report'
+    msg['From'] = from_email
+    msg['To'] = ', '.join(to_emails)
+    msg.preamble = "Here's the weekly KPI Report"
+
+    with open('report.png', 'rb') as fp:
+        img = MIMEImage(fp.read())
+    msg.attach(img)
+
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
+if __name__ == '__main__':
+    send_email('matt.warren@gmail.com', ['matt.warren@gmail.com'])
