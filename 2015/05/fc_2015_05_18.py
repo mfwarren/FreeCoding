863fcca66840f0f7e9606445639bf77861e746ac
Matt Warren <matt.warren@gmail.com>
2015-05-21 22:18:15 -0600

    exponential growth is powerful

--- 2015/05/fc_2015_05_18.py
+++ 2015/05/fc_2015_05_18.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # imports go here
+import plotly.plotly as py
+from plotly.graph_objs import *
 
 #
 # Free Coding session for 2015-05-18
@@ -24,8 +26,12 @@ for i in range(YEARS_TO_SIMULATE):
         cost = cost_per_watt[i-1] * (1+INFLATION_RATE)
     cost_per_watt.append(cost)
 
+revenue_per_kwh = [KWH_PRICE, ]
+for i in range(YEARS_TO_SIMULATE):
+    revenue_per_kwh.append(revenue_per_kwh[i] * (1+INFLATION_RATE))
+
 EARLY_GROWTH_RATE_YOY = 0.6
-LATE_GROWTH_RATE_YOY = 0.1
+LATE_GROWTH_RATE_YOY = 0.01
 INSTALLS_IN_FIRST_YEAR = 200
 
 installs_per_year = [INSTALLS_IN_FIRST_YEAR, ]
@@ -52,22 +58,23 @@ for i in range(YEARS_TO_SIMULATE):
 print(captial_requirements_per_year)
 
 
-daily_output = energy_output_avg_per_hour_rate * daylight_hours_per_day * expected_efficiency * avg_home_install_watts
-monthly_output = (daily_output * 365) / 12.0
-monthly_revenue = (monthly_output / 1000.0) * KWH_PRICE
+daily_output_kwh = energy_output_avg_per_hour_rate * daylight_hours_per_day * expected_efficiency * avg_home_install_watts / 1000
+print("Daily output KWH: %s", daily_output_kwh)
+monthly_output = (daily_output_kwh * 365) / 12.0
+monthly_revenue = monthly_output * KWH_PRICE
 roi = (avg_install_cost_per_house / monthly_revenue) / 12.0
 
-watt_hour_production_per_year = [INSTALLS_IN_FIRST_YEAR/2*daily_output*365]
+kwh_production_per_year = [(INSTALLS_IN_FIRST_YEAR/2)*daily_output_kwh*365]
 for i in range(YEARS_TO_SIMULATE):
-    prev_year_base = watt_hour_production_per_year[i]*2
-    avg_from_this_year = installs_per_year[i+1]/2*daily_output*365
+    prev_year_base = kwh_production_per_year[i]*2
+    avg_from_this_year = (installs_per_year[i+1]/2)*daily_output_kwh*365
     total = prev_year_base + avg_from_this_year
-    watt_hour_production_per_year.append(total)
+    kwh_production_per_year.append(total)
 
-print(watt_hour_production_per_year)
+print(kwh_production_per_year)
 
 print("total Install cost: $%s" % avg_install_cost_per_house)
-print('Daily output (kWh): %s' % (daily_output / 1000.0))
+print('Daily output (kWh): %s' % (daily_output_kwh))
 print("monthly production (kWh): %s" % (monthly_output / 1000.0))
 print("monthly revenue: $%s" % monthly_revenue)
 print("ROI (years) %s" % roi)
@@ -79,9 +86,10 @@ print("annual revenue: $%s" % (monthly_revenue*12))
 interest_rate = 0.02
 
 debt_principal = [avg_install_cost_per_house, ]
-annual_revenue = [(monthly_revenue * 12), ]
+annual_revenue = []
 for i in range(YEARS_TO_SIMULATE):
-    annual_revenue.append(annual_revenue[i]*(1+INFLATION_RATE))
+    annual_revenue.append(revenue_per_kwh[i]*kwh_production_per_year[i])
+print(annual_revenue)
 
 interest_due = []
 for i in range(YEARS_TO_SIMULATE):
@@ -91,3 +99,24 @@ for i in range(YEARS_TO_SIMULATE):
     debt_principal.append(debt_principal[i] - profit)
 
 print(debt_principal)
+
+
+line = Scatter(x=list(range(2015, 2015+YEARS_TO_SIMULATE)), y=annual_revenue)
+line2 = Scatter(x=list(range(2015, 2015+YEARS_TO_SIMULATE)), y=installs_per_year)
+line3 = Scatter(x=list(range(2015, 2015+YEARS_TO_SIMULATE)), y=kwh_production_per_year)
+data = Data([line, line2])
+layout = Layout(
+    title='Revenue per year under contract',
+    xaxis=XAxis(
+        title='Year',
+        showgrid=False,
+        zeroline=False
+    ),
+    yaxis=YAxis(
+        title='Revenue $',
+        showline=False
+    )
+)
+fig = Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='Revenue from contracts')
+print(plot_url)
