d2376a5d88baf5f38992800e9561cf036a81f87a
Matt Warren <matt.warren@gmail.com>
2015-06-06 22:01:53 -0600

    a linear regression using scikit-learn

---
+++ 2015/06/fc_2015_06_06.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# imports go here
+from sklearn import datasets
+from sklearn.cross_validation import cross_val_predict
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+#
+# Free Coding session for 2015-06-06
+# Written by Matt Warren
+#
+
+lr = linear_model.LinearRegression()
+boston = datasets.load_boston()
+y = boston.target
+
+predicted = cross_val_predict(lr, boston.data, y, cv=10)
+
+
+fig, ax = plt.subplots()
+ax.scatter(y, predicted)
+ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+ax.set_xlabel('Measured')
+ax.set_ylabel('Predicted')
+fig.show()
+print(predicted)
