17eb2ed3dff819591e41e10b388af7d1306a696c
Matt Warren <matt.warren@gmail.com>
2015-06-09 22:41:04 -0600

    ward hierarchical clustering with image data

---
+++ 2015/06/fc_2015_06_09.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# imports go here
+
+import time as time
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.image import grid_to_graph
+from sklearn.cluster import AgglomerativeClustering
+
+#
+# Free Coding session for 2015-06-09
+# Written by Matt Warren
+#
+
+lena = sp.misc.lena()
+X = np.reshape(lena, (-1, 1))
+print(X)
+
+connectivity = grid_to_graph(*lena.shape)
+
+st = time.time()
+n_clusters = 15  # number of regions
+ward = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward', connectivity=connectivity).fit(X)
+label = np.reshape(ward.labels_, lena.shape)
+
+plt.figure(figsize=(5, 5))
+plt.imshow(lena, cmap=plt.cm.gray)
+for l in range(n_clusters):
+    plt.contour(label == l, contours=1,
+                colors=[plt.cm.spectral(l / float(n_clusters)), ])
+plt.xticks(())
+plt.yticks(())
+plt.show()
