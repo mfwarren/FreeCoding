8318c08b24374090dafd9363ebee14e113373dd1
Matt Warren <matt.warren@gmail.com>
2015-06-26 22:05:24 -0600

    have to migrate 9 websites tonight to a new server.  no time to spare

---
+++ 2015/06/fc_2015_06_26.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# imports go here
+import time
+
+#
+# Free Coding session for 2015-06-26
+# Written by Matt Warren
+#
+
+start_time = time.time()
+
+print("too much to do")
+
+elapsed_time = time.time() - start_time
+
+if elapsed_time > 0:
+    print("that took too long.  no time to spare - get back to work!")
